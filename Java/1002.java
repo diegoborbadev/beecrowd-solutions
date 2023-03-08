1f71fa85b32e7b8bdd61300e2a993865a4d3c8d5
Diego Borba <diegoborba25.contato@gmail.com>
2023-03-08 12:27:04 -0300

    Code errors fixed

--- Java/1002.java
+++ Java/1002.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
--- Java/1308.java
+++ Java/1308.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         
         int n = scanner.nextInt();
