d032c21b10a7ca37121ae545d1fb6376162ba9c1
Diego Borba <diegoborba25.contato@gmail.com>
2023-06-26 21:38:20 -0300

    1011 (Java)

---
+++ Java/1011.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        double raio = scanner.nextDouble();
+
+        double volume = (4.0 / 3.0) * 3.14159 * Math.pow(raio, 3);
+
+        System.out.printf("VOLUME = %.3f%n", volume);
+
+        scanner.close();
+    }
+}
