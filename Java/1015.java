5e0a0186c35deb48234280ad48bcfa36b17e1140
Diego Borba <diegoborba25.contato@gmail.com>
2023-08-23 23:18:41 -0300

    1015 (Java)

---
+++ Java/1015.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        double x1 = scanner.nextDouble();
+        double y1 = scanner.nextDouble();
+        double x2 = scanner.nextDouble();
+        double y2 = scanner.nextDouble();
+
+        System.out.printf("%.4f%n", Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+
+        scanner.close();
+    }
+}
