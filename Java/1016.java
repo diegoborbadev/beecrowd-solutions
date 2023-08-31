15c86f9f5a095715483f7cc73ada82c40158ea50
Diego Borba <diegoborba25.contato@gmail.com>
2023-08-30 22:15:29 -0300

    1016 (Java)

---
+++ Java/1016.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println(scanner.nextInt() + " minutos");	
+
+        scanner.close();
+    }
+}
