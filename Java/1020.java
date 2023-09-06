b505a50704f7b3f298eeb8b93a88e81ff1e252de
Diego Borba <diegoborba25.contato@gmail.com>
2023-09-05 23:08:36 -0300

    1020 (Java)

---
+++ Java/1020.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		
+		int d;
+        int m;
+        int y;
+        
+		d = scanner.nextInt();
+		
+		if (d >= 365){
+			y = d / 365;
+			d = d % 365;
+			System.out.printf("%d ano(s)\n", y);
+		} else{
+			System.out.printf("0 ano(s)\n");
+		}
+		
+		if (d >= 30){
+			m = d / 30;
+			d = d % 30;
+			System.out.printf("%d mes(es)\n", m);
+		} else{
+			System.out.printf("0 mes(es)\n");
+		}
+		
+		System.out.printf("%d dia(s)\n", d);
+
+		scanner.close();
+    }
+}
