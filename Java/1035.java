716943ff447a8d632b846c6f79fd594e5cde8df4
Diego Borba <diegoborba25.contato@gmail.com>
2023-09-07 20:19:20 -0300

    1035 (Java)

---
+++ Java/1035.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        
+		int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        
+		if (b > c && d > a && c > 0 && d > 0 && (c + d) > (a + b) && a % 2 == 0) {
+            System.out.println("Valores aceitos"); 
+        } else { 
+            System.out.println("Valores nao aceitos");
+        }
+
+		scanner.close();
+    }
+}
