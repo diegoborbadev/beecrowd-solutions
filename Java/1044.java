95a3c3ab09e2df2dc1f8a6eaee3addb4bab891aa
Diego Borba <diegoborba25.contato@gmail.com>
2023-09-12 22:05:35 -0300

    1044 (Java)

---
+++ Java/1044.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        if (a % b == 0 || b % a == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+
+        scanner.close();
+    }
+}
