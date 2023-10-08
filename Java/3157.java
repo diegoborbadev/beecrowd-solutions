d1f319be09a09c8b88dd6fa5e865f45d725cd811
Diego Borba <diegoborba25.contato@gmail.com>
2023-10-07 23:06:05 -0300

    3157 (Java)

---
+++ Java/3157.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		new Scanner(System.in).nextLine();
+        System.out.println("Nao se refuta ciencia com opiniao");
+	}
+}
