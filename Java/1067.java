7af12f9261226b4190c1db883a2771b5b72373d5
Diego Borba <diegoborba25.contato@gmail.com>
2023-10-17 23:47:20 -0300

    1067 (Java)

---
+++ Java/1067.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		for (int i = 1; i < n; i+=2) {
+			System.out.println(i);
+		}
+	}
+}
