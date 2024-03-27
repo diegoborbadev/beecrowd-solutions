f8c90ea5611bd6ee316f1d30951542a3d67c4e5b
Diego Borba <diegoborba25.contato@gmail.com>
2024-03-26 23:02:24 -0300

    3146 (Elixir/Kotlin)

---
+++ Elixir/3146.ex
@@ -0,0 +1,1 @@
+IO.puts(:io_lib.format("~.2f", [String.to_float(IO.gets("") |> String.trim) * 6.28]))
---
+++ Kotlin/3146.kt
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`);
+    println("%.2f".format(scanner.nextFloat() * 6.28))
+}
