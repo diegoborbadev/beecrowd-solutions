f4dfca97d7cd764d64e71bc25c235364967ca1fc
Diego Borba <diegoborba25.contato@gmail.com>
2024-03-27 10:26:39 -0300

    3224 (C++/JavaScript)

---
+++ C++/3224.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string j, d;
+    cin >> j >> d;
+
+    (j.size() >= d.size()) ? cout << "go\n" : cout << "no\n";
+
+    return 0;
+}
