45275631f46bbd7c119d1dcc795932a1d8270b4f
Diego Borba <diegoborba25.contato@gmail.com>
2024-03-29 01:59:26 -0300

    1005 (PHP/Python)

---
+++ PHP/1005.php
@@ -0,0 +1,5 @@
+<?php
+    $a = trim(fgets(STDIN));
+    $b = trim(fgets(STDIN));
+    printf("MEDIA = %.5f\n", ($a * 3.5 + $b * 7.5) / 11);
+?>
---
+++ Python/1005.py
@@ -0,0 +1,3 @@
+a = float(input())
+b = float(input())
+print(f"MEDIA = {(a * 3.5 + b * 7.5) / 11:.5f}")
