f0fe62e76eed65e47d889ce876ab8df8119d4bda
Diego Borba <diegoborba25.contato@gmail.com>
2024-03-29 11:57:32 -0300

    1005 (Swift)

--- Swift/1005.swift
+++ Swift/1005.swift
@@ -1,3 +1,6 @@
-let a = Double(readLine(strippingNewline: true)!)!
-let b = Double(readLine(strippingNewline: true)!)!
-print(String(format: "MEDIA = %.5f", (a * 3.5 + b * 7.5) / 11))
+import Foundation
+
+if let inputA = readLine(), let inputB = readLine(), let a = Float(inputA), let b = Float(inputB) {
+    let r = (a * 3.5 + b * 7.5) / 11.0
+    print(String(format: "MEDIA = %.5f",  r))
+}
