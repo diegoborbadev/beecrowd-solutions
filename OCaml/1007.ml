6042e7c1161cee6f98406e1a36eb3d17f2206b66
Diego Borba <diegoborba25.contato@gmail.com>
2024-03-29 12:29:10 -0300

    1007 (Lua/OCaml/Pascal)

---
+++ OCaml/1007.ml
@@ -0,0 +1,3 @@
+Scanf.scanf "%i\n %i\n %i\n %i" (fun a b c d ->
+    Printf.printf "DIFERENCA = %d\n" (a * b - c * d);
+)
---
+++ Pascal/1007.pas
@@ -0,0 +1,12 @@
+var
+    a : int64;
+    b : int64;
+    c : int64;
+    d : int64;
+begin
+    read(a);
+    read(b);
+    read(c);
+    read(d);
+    writeln('DIFERENCA = ', a * b - c * d);
+end.
