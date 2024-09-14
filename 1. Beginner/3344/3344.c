e3c63385b436679b92d2138f9eb40c82906e843b
Diego Borba <diegoborba25.contato@gmail.com>
2024-09-13 23:41:21 -0300

    3344 (C/Clojure/C++)

---
+++ 1. Beginner/3344/3344.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("4\n");
+    return 0;
+}
---
+++ 1. Beginner/3344/3344.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+ 
+using namespace std;
+
+int main() {
+    cout << "4" << endl;
+    return 0;
+}
