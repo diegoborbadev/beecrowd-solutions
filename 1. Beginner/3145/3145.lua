730147d67e5a087f2e8ae16c14d695dda89d94a0
Diego Borba <diegoborba25.contato@gmail.com>
2024-09-13 23:39:46 -0300

    3145 (Lua/OCaml/Pascal)

---
+++ 1. Beginner/3145/3145.lua
@@ -0,0 +1,3 @@
+n = tonumber(io.read())
+x = tonumber(io.read())
+print(string.format("%.2f", x / (n + 2)))
---
+++ 1. Beginner/3145/3145.ml
@@ -0,0 +1,3 @@
+Scanf.scanf "%f" (fun r ->
+    Printf.printf "A=%.4f\n" (r *. r *. 3.14159);
+)
